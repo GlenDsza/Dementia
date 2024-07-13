ae65abc5d35087a9d61bd0ad2bad9f2f52140006
Glen <dsouzaglen30@gmail.com>
2024-07-13 18:38:44 +0530

    Map base

---
+++ App/README.txt
@@ -0,0 +1,18 @@
+npm install -g @capacitor/cli (for cap commands)
+
+- To run on android (requires android studio setup)
+    - npm run build
+    - npx cap sync android
+    - npx cap open android
+
+- To run on ios (requires xcode setup)
+    - npm run build
+    - npx cap sync ios
+    - npx cap open ios
+
+- For Capacitor google-maps
+    - We are waiting for a release of some google maps native dependencies to add Swift Package Manager support and ship with everything up to date.
+
+    -For the moment you can install the RC version with npm install @capacitor/google-maps@next, but have in mind that the final version will require iOS 14 (while Capacitor itself still allows iOS 13) because of the google maps dependencies requiring iOS 14 or newer.
+
+- Add GOOGLE_MAPS_API_KEY=YOUR_API_KEY in android/.env
--- App/src/pages/Map/Map.tsx
+++ App/src/pages/Map/Map.tsx
@@ -1,18 +1,107 @@
-import { IonContent, IonHeader, IonPage, IonToolbar } from "@ionic/react";
-import React from "react";
+import { GoogleMap } from "@capacitor/google-maps";
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonProgressBar,
+  IonToolbar,
+} from "@ionic/react";
+import React, { FC, useEffect, useRef, useState } from "react";
+import "./Map.css";
 
-const Map = () => {
+const markers = [
+  {
+    title: "Mumbai",
+    snippet: "Hello World :D",
+    // iconUrl: "assets/icon/icon1.png",
+    // iconSize: {
+    //   width: 35,
+    //   height: 35,
+    // },
+    coordinate: {
+      lat: 19.075983,
+      lng: 72.877655,
+    },
+  },
+  {
+    title: "Yakima",
+    snippet: "City",
+    // iconUrl: "assets/icon/icon2.png",
+    // iconSize: {
+    //   width: 35,
+    //   height: 35,
+    // },
+    coordinate: {
+      lat: 46.60207,
+      lng: -120.505898,
+    },
+  },
+];
+
+const Map: FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const mapRef = useRef<HTMLElement>();
+  let newMap: GoogleMap;
+
+  const createMap = async () => {
+    setLoading(true);
+    if (!mapRef.current) {
+      setLoading(false);
+      return;
+    }
+
+    newMap = await GoogleMap.create({
+      id: "my-map",
+      element: mapRef.current,
+      apiKey: import.meta.env.VITE_GMAPS_KEY,
+      config: {
+        center: {
+          lat: 19.075983,
+          lng: 72.877655,
+        },
+        zoom: 10,
+      },
+    });
+
+    markers.forEach((marker) => {
+      newMap.addMarker(marker);
+    });
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    createMap();
+  }, []);
+
   return (
     <IonPage>
-      <IonContent fullscreen className="ion-padding">
-        <IonHeader>
-          <IonToolbar style={{ borderRadius: "1rem" }}>
-            <div className="title">
-              <img src="/favicon.png" alt="Logo" width={30} height={30} />
-              <h1>Dementia 101</h1>
-            </div>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent fullscreen>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton
+              type="button"
+              className="backbutton"
+              defaultHref="/home"
+            ></IonBackButton>
+          </IonButtons>
+          <div className="title">
+            <img src="/favicon.png" alt="Logo" width={30} height={30} />
+            <h1>Dementia 101</h1>
+          </div>
+          <IonProgressBar type="determinate"></IonProgressBar>
+        </IonToolbar>
+
+        <capacitor-google-map
+          ref={mapRef}
+          style={{
+            display: "inline-block",
+            width: "100%",
+            height: "100%",
+          }}
+        ></capacitor-google-map>
       </IonContent>
     </IonPage>
   );
