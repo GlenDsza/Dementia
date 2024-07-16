62f484880b8565f7d4b2375e4e1db2273777c3c2
kanika296 <kanikaofficial.517191@gmail.com>
2024-07-16 13:25:52 +0530

    Dashboard: routine and location

---
+++ newApp/src/models/Todo.ts
@@ -0,0 +1,12 @@
+// src/models/Todo.ts
+export interface Todo {
+    id: number;
+    name: string;
+    time: string;
+    activity: string;
+    location: string;
+    image: string;
+    mapUrl: string;
+    completed: boolean;
+  }
+  
