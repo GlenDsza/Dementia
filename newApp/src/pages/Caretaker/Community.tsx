69c8824bbaacc7754343163de8d14a228ea4ae78
kanika296 <kanikaofficial.517191@gmail.com>
2024-07-17 00:32:05 +0530

    added groups

--- newApp/src/pages/Caretaker/Community.tsx
+++ newApp/src/pages/Caretaker/Community.tsx
@@ -14,91 +14,77 @@ import {
     IonCol,
     IonRow,
     IonButton,
+    IonImg,
   } from '@ionic/react';
   import React from 'react';
   import './Community.scss';
   
   const mockData = [
     {
-      name: 'John Doe',
-      description: 'Software Engineer at XYZ Company.',
-      image: 'https://randomuser.me/api/portraits/men/1.jpg'
+      name: 'Dementia Care Circle',
+      description: 'Sharing experiences and tips in dementia care.',
+      image: 'https://img.freepik.com/premium-vector/cute-elephant-logo-icon-vector-character_760408-50.jpg'
     },
     {
-      name: 'Jane Smith',
-      description: 'Marketing Specialist at ABC Inc.',
-      image: 'https://randomuser.me/api/portraits/women/1.jpg'
+      name: 'Memory Lane Companions',
+      description: 'A space to discuss and share memory care strategies.',
+      image: 'https://play-lh.googleusercontent.com/Jrnpp_Um2QspmwhSnG9WHgBZXvXN-Z7mbjlAJyaZxI--Prvzemx3Qkjz23io7y8-JlM'
     },
     {
-      name: 'Sam Wilson',
-      description: 'Project Manager at DEF Corp.',
-      image: 'https://randomuser.me/api/portraits/men/2.jpg'
+      name: 'Caring Hearts Network',
+      description: 'Dedicated to compassionate dementia caregiving.'      ,
+      image: 'https://media.istockphoto.com/id/1170794027/vector/yin-yang-panda-cute-logo-vector-illustration.jpg?s=612x612&w=0&k=20&c=xiWk8WaN9dyqRSsptwU_RAckfFiiTdOPzbtuxzJfynY='
     },
     {
-      name: 'Sara Connor',
-      description: 'UX Designer at GHI Ltd.',
-      image: 'https://randomuser.me/api/portraits/women/2.jpg'
+      name: 'Mindful Caregivers',
+      description: 'Mindful approaches to dementia care.',
+      image: 'https://static.vecteezy.com/system/resources/previews/021/730/507/original/rainbow-logo-cute-hand-drawn-flat-design-modern-colors-free-vector.jpg'
     },
     {
-        name: 'John Doe',
-        description: 'Software Engineer at XYZ Company.',
-        image: 'https://randomuser.me/api/portraits/men/1.jpg'
+        name: 'Supportive Hands',
+        description: '',
+        image: 'https://logowik.com/content/uploads/images/cute-peeking-dog8044.logowik.com.webp'
       },
       {
-        name: 'Jane Smith',
-        description: 'Marketing Specialist at ABC Inc.',
-        image: 'https://randomuser.me/api/portraits/women/1.jpg'
+        name: 'Dementia Allies',
+        description: '',
+        image: 'https://cdn5.vectorstock.com/i/1000x1000/61/19/cute-cartoon-unicorn-fantasy-logo-vector-27996119.jpg'
       },
       {
-        name: 'Sam Wilson',
-        description: 'Project Manager at DEF Corp.',
-        image: 'https://randomuser.me/api/portraits/men/2.jpg'
+        name: 'Dementia Caregivers United',
+        description: '',
+        image: 'https://img.freepik.com/premium-vector/cute-peace-hand-cartoon-vector-icon-illustration-logo-mascot-hand-drawn-concept-trandy-cartoon_519183-176.jpg'
       },
       {
-        name: 'Sara Connor',
-        description: 'UX Designer at GHI Ltd.',
-        image: 'https://randomuser.me/api/portraits/women/2.jpg'
+        name: 'Memory Keepers',
+        description: '',
+        image: 'https://i.pinimg.com/736x/54/fa/ca/54facabf95a6b95acd2d085deb505816.jpg'
       },
       {
-        name: 'John Doe',
-        description: 'Software Engineer at XYZ Company.',
-        image: 'https://randomuser.me/api/portraits/men/1.jpg'
-      },
-      {
-        name: 'Jane Smith',
-        description: 'Marketing Specialist at ABC Inc.',
-        image: 'https://randomuser.me/api/portraits/women/1.jpg'
-      },
-      {
-        name: 'Sam Wilson',
-        description: 'Project Manager at DEF Corp.',
-        image: 'https://randomuser.me/api/portraits/men/2.jpg'
-      },
-      {
-        name: 'Sara Connor',
-        description: 'UX Designer at GHI Ltd.',
-        image: 'https://randomuser.me/api/portraits/women/2.jpg'
-      },
-      {
-          name: 'John Doe',
-          description: 'Software Engineer at XYZ Company.',
-          image: 'https://randomuser.me/api/portraits/men/1.jpg'
+          name: 'Gentle Care Network',
+          description: '',
+          image: 'https://c8.alamy.com/comp/2G9PA3A/cute-funny-happy-star-sign-character-vector-hand-drawn-cartoon-kawaii-character-illustration-icon-isolated-on-white-background-star-rating-symbol-review-doodle-character-concept-2G9PA3A.jpg'
         },
         {
-          name: 'Jane Smith',
-          description: 'Marketing Specialist at ABC Inc.',
-          image: 'https://randomuser.me/api/portraits/women/1.jpg'
+          name: "Caregiver's Oasis",
+          description: '',
+          image: 'https://i.pinimg.com/736x/86/79/be/8679be5b00e470cd84e5a19513451d52.jpg'
         },
         {
-          name: 'Sam Wilson',
-          description: 'Project Manager at DEF Corp.',
-          image: 'https://randomuser.me/api/portraits/men/2.jpg'
+          name: 'Hope and Healing',
+          description: '',
+          image: 'https://thumbs.dreamstime.com/b/caring-hands-team-vector-illustration-logo-vector-illustration-caring-hands-team-suitable-logo-white-background-88084233.jpg'
         },
-        {
-          name: 'Sara Connor',
-          description: 'UX Designer at GHI Ltd.',
-          image: 'https://randomuser.me/api/portraits/women/2.jpg'
-        },
+      {
+        name: 'Memory Care Network',
+        description: '',
+        image: 'https://st4.depositphotos.com/16921356/20303/v/450/depositphotos_203031124-stock-illustration-cute-owl-logo-vector-dsign.jpg'
+      },
+      {
+        name: 'Peaceful Minds',
+        description: '',
+        image: 'https://www.shutterstock.com/image-vector/teamwork-logo-abstract-two-hands-260nw-2282414665.jpg'
+      }
     // Add more mock data as needed
   ];
   
@@ -110,10 +96,11 @@ import {
             <IonButtons slot="start">
               <IonMenuButton />
             </IonButtons>
-            <IonTitle>Community Members</IonTitle>
+            <IonTitle>Community</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen className="avatars-list-content">
+          <IonImg src= 'https://static.vecteezy.com/system/resources/thumbnails/007/509/583/small/lively-characters-of-old-age-flat-design-style-illustration-free-vector.jpg'/>
           {mockData.map((person, index) => (
             <IonCard key={index} className="avatars-card">
                 <IonRow>
