d575868498a666450563929c595724414dc1e9d1
Harit Sharma <harit2101@gmail.com>
2024-07-19 04:32:24 +0530

    login page css

--- newApp/capacitor.config.ts
+++ newApp/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'dementia_1',
+  appName: 'Reva',
   webDir: 'dist',
   plugins: {
     LocalNotifications: {
